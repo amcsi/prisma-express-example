3f04b3998717e6fea2c3d0fcc87803d2f3eeb435
Attila Szeremi <attila@szeremi.org>
2023-08-02 22:38:28 +0200

    Create and get users

--- backend.ts
+++ backend.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 // Create an Express app
 const app = express();
@@ -10,10 +13,22 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-app.post('/api/users', (req, res) => {
-  console.log(req.body);
+app.post('/api/users', async (req, res) => {
+  const body = req.body;
+  const user = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  })
 
-  res.send('');
+  res.json(user);
+});
+
+app.get('/api/users', async (req, res) => {
+  const users = await prisma.user.findMany()
+
+  res.json(users);
 });
 
 // Set up the server to listen on port 3000
